87786fae45638303e9facbdd92ea93f563938113
ali <moini_alessandro_13@yahoo.com>
2024-09-19 11:00:43 +0430

    change test build

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,9 @@ mongoose
   .catch((err) => console.log(err));
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
 );
 
 const server = http.createServer(app);
